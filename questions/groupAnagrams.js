49634cf24e05283d431cd0aa0e41112d191e1714
Daksh Singh <daksh.singh.2407@gmail.com>
2026-02-06 11:13:24 +0530

    6th February 2026 commit 2

---
+++ questions/groupAnagrams.js
@@ -0,0 +1,14 @@
+const arr = ["eat", "tea", "tan", "ant", "bat"];
+let out_arr = {};
+arr.forEach((ele) => {
+    sp_el = ele.split("").sort();
+    if (!out_arr[sp_el]) {
+    out_arr[sp_el] = [ele];
+    } else {
+    out_arr[sp_el].push(ele);
+    }
+});
+
+for (const [key] of Object.entries(out_arr)) {
+    console.log(out_arr[key].join(" "));   
+}
