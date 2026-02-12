1344b024376b059ff78823cd31808f5fbb9c3082
Daksh Singh <daksh.singh.2407@gmail.com>
2026-02-12 13:06:49 +0530

    12th February 2026 commit 2

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
-const obj = {
-    a: 10,
-    b: 20,
-    sum() {
-        return this.a + this.b;
-    }
+// const obj = {
+//     a: 10,
+//     b: 20,
+//     sum() {
+//         return this.a + this.b;
+//     }
+// }
+
+// const res = obj.sum.bind(obj);
+// console.log(res());
+
+function check() { throw new Error("Parameter required") }
+
+function displayDetails(name = check()) {
+    if (name == 'N/A') { throw Error }
+    console.log(name);
 }
 
-const res = obj.sum.bind(obj);
-console.log(res());
+displayDetails();
